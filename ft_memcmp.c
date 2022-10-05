046ce5cc8e72570b0f824869e8a41c22851aa464
Mustafa Karadeniz <mkaraden@k1m16s07.42istanbul.com.tr>
2022-10-05 16:04:16 +0300

    .

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mkaraden < mkaraden@student.42istanbul.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:46:52 by mkaraden          #+#    #+#             */
-/*   Updated: 2022/10/05 15:19:48 by mkaraden         ###   ########.fr       */
+/*   Updated: 2022/10/05 16:03:29 by mkaraden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	while (i < n)
 	{
-		if ((unsigned char *)(s1 + i) != (unsigned char *)(s2 + i))
+		if (*(str1 + i) != *(str2 + i))
 		{
-			return ((unsigned char *)(s1 + i) - (unsigned char *)(s2 + i));
+			return (*(str1 + i) - *(str2 + i));
 		}
 		i++;
 	}
