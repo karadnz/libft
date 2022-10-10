48257c7c160052a84c8422b4a3043be5d7c2653a
Mustafa Karadeniz <mkaradeniz@mair.local>
2022-10-10 13:45:49 +0300

    added split

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkaraden <mkaraden@student.42istanbul.com  +#+  +:+       +#+        */
+/*   By: mkaraden <mkaraden@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 16:52:22 by mkaraden          #+#    #+#             */
-/*   Updated: 2022/10/08 18:31:40 by mkaraden         ###   ########.fr       */
+/*   Updated: 2022/10/10 13:45:32 by mkaraden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ static int	ft_getwordcount(char const *s, char c)
 	rt = 0;
 	while(s[i])
 	{
-		while (s[i] == c)
+		while (s[i] == c)  //c leri geciyor
 			i++;
-		if(s[i] != c && s[i])
+		if(s[i] != c && s[i]) //sona gelmediyse rt artiyor
 			rt++;
-		while (s[i] != c && s[i])
+		while (s[i] != c && s[i]) //c gelene kadar index artiyor
 			i++;
 	}
 	return (rt);
@@ -36,26 +36,31 @@ char	**ft_split(char const *s, char c)
 	char	**rt;
 	int		wordcount;
 	int		i;
-	int		j;
+	int		templen; //wordlen
+	int		j; //dis arrayin indexi
 
 	i = 0;
+	j = -1;
 	wordcount = ft_getwordcount(s,c);
-	rt = (char **)malloc(sizeof(char *) * wordcount);
+	rt = (char **)malloc(sizeof(char *) * (wordcount + 1)); //sondaki null pointer icin
 
-	while (s[i])
+	while(s[i])
 	{
-		j = 0;
-		while (s[i] == c)
+		templen = 0;
+		while (s[i] == c)  //c leri geciyor
 			i++;
-		if(s[i] != c && s[i])
-		{
-			while(s[i + j] != c && s[i + j])
-			{
-				j++;
-			}
-			while(j > 0)
-			{
-				rt[
+		if(s[i] != c && s[i]) //sona gelmediyse rt artiyor
+			j++;
+		while (s[i + templen] != c && s[i + templen]) //c gelene kadar index artiyor
+			templen++;
+		if(templen > 0)
+			rt[j] = ft_substr(s, i, templen); //123 456 789  \0 durumunda sondaki karakteri substr ye gondermemesi icin
+		i+=templen;
+	}
+	j++; //dongu icinde son kelimeden sonra artmiyor
+	rt[j] = 0;
+	return (rt);
+}
 
 
 
--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkaraden <mkaraden@student.42istanbul.com  +#+  +:+       +#+        */
+/*   By: mkaraden <mkaraden@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 13:20:43 by mkaraden          #+#    #+#             */
-/*   Updated: 2022/10/08 13:31:03 by mkaraden         ###   ########.fr       */
+/*   Updated: 2022/10/10 13:45:34 by mkaraden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*rt;
 	size_t	i;
+	int		slen; //condition icinde fonk olarak karsilastirince her loopda bastan cagiriyor
 
+	slen = ft_strlen(s);
 	i = 0;
 	rt = (char *)malloc(len + 1);
-	if (!s || !rt)
+	if (!s || !rt) //eger s duzgunse rt ye bakmiyor ama rt ye bakmasi gerekince sikinti cikiyor
 		return (0);
-	while (i < ft_strlen(s) && i < len)
+	while (i < slen && i < len)
 		rt[i++] = s[start++];
 	rt[i] = '\0';
 	return (rt);
